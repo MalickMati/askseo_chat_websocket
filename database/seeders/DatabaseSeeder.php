ab76900e2d9973e99eb4b44493b5781d06b1b3f1
MalickMati <malikmati293@gmail.com>
2025-08-12 11:01:38 +0500

    Full Project Push

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,31 +12,19 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        User::create([
-            'name' => 'CEO',
-            'email' => 'ceo@askseo.com',
-            'password' => Hash::make('password123'),
-            'status' => 'active',  
-            'status_mode' => 'offline', 
-            'image' => null,  
-            'type' => 'super_admin', 
-        ]);
+        $path = database_path('seeders/seed.sql');
+
+        // Check if the file exists
+        if (File::exists($path)) {
+            // Get the contents of the SQL file
+            $sql = File::get($path);
+
+            // Execute the SQL queries
+            DB::unprepared($sql);
+
+            $this->command->info('SQL Seed file executed successfully.');
+        } else {
+            $this->command->error('SQL file not found.');
+        }
     }
-    // public function run(): void
-    // {
-    //     $path = database_path('seeders/seed.sql');
-
-    //     // Check if the file exists
-    //     if (File::exists($path)) {
-    //         // Get the contents of the SQL file
-    //         $sql = File::get($path);
-
-    //         // Execute the SQL queries
-    //         DB::unprepared($sql);
-
-    //         $this->command->info('SQL Seed file executed successfully.');
-    //     } else {
-    //         $this->command->error('SQL file not found.');
-    //     }
-    // }
 }
